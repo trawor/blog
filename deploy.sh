4fbead1455bbfb33b3eb499062f3aea119682e08
Travis Wang <tw@travis.wang>
2026-01-08 11:34:14 +0800

    not need dockerfile

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-docker build -t hub.home.dev/blog/tw ./
-docker push hub.home.dev/blog/tw
-curl -X PUT \
-  -H "Content-Type: application/yaml" \
-  -H "Cookie: KuboardUsername=admin; KuboardAccessKey=wphzwbi8df4f.ybs2pcye7p2mz6in5bez42jtdibi6iai" \
-  -d '{"kind":"deployments","namespace":"default","name":"twblog"}' \
-  "https://board.home.dev/kuboard-api/cluster/default/kind/CICDApi/admin/resource/restartWorkload"
